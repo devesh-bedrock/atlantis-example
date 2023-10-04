c35ae323f77a7578efa49739546e6bf7a9635b45
Devesh Yamparala <devesh@bedrock.security>
2023-10-03 22:28:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "bedrock_security" {
   source = "gcs::https://www.googleapis.com/storage/v1/f9ce9e68-c2ff-467c-b7d4-8ce488d986e5/module-v1.0.1.zip"
   region       = "us-central1"
   project_id   = "persona-web-staging"
-  customer_key = "cebdeda8-1685-3c99-8ef0-d00601488232"
-  starting_prefix = "bedrock-39fa98"
+  customer_key = "gcptestcust1"
+  starting_prefix = "bedrock-9decd0"
   public_bucket   = "f9ce9e68-c2ff-467c-b7d4-8ce488d986e5"
 }
 resource "null_resource" "bedrock_security_create_success" {
