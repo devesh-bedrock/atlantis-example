2e92580e4bfaeefc618977eacee07709a3590f23
Devesh Yamparala <devesh@bedrock.security>
2023-10-04 00:35:09 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,1 @@
-module "bedrock_security" {
-  source = "gcs::https://www.googleapis.com/storage/v1/f9ce9e68-c2ff-467c-b7d4-8ce488d986e5/devesh-module.zip"
-  region       = "us-central1"
-  project_id   = "devesh-atlantis-test"
-  customer_key = "gcptestcust1"
-  starting_prefix = "bedrock-9decd0"
-  public_bucket   = "f9ce9e68-c2ff-467c-b7d4-8ce488d986e5"
-}
+
