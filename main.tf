c3dfcddb89107dd14b16e3a0103ade7fca6077c8
Devesh Yamparala <devesh@bedrock.security>
2023-10-04 00:01:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,3 @@ module "bedrock_security" {
   starting_prefix = "bedrock-9decd0"
   public_bucket   = "f9ce9e68-c2ff-467c-b7d4-8ce488d986e5"
 }
-resource "null_resource" "bedrock_security_create_success" {
-  provisioner "local-exec" {
-    command = "gcloud pubsub topics publish ${module.bedrock_security.starting_prefix}-terraform-status-queue --message=\"message\" --attribute=\"terraform_status=SUCCESS\" --project ${module.bedrock_security.project_id}"
-  }
-  depends_on = [module.bedrock_security]
-}
